230c73751f64a40845d74d616ebeda7e32343bbb
reem-masoud <65132189+reem-masoud@users.noreply.github.com>
2020-09-30 12:15:03 +0300

    Update main.go

--- back-end/main.go
+++ back-end/main.go
@@ -7,23 +7,13 @@ import (
 	"html"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/rs/cors"
+	"github.com/gorilla/mux"
 	"github.com/service-monitor/back-end/timeresponse"
 )
-
-type Person struct {
-	ID  int    `json:"id,omitempty"`
-	Url string `json:"firstname,omitempty"`
-}
-
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
 
 type Item struct {
 	Id        int       `json:"id"`
@@ -31,6 +21,7 @@ type Item struct {
 	Timeresp  Duration  `json:"timeresponse"`
 	Num       int       `json:"code"`
 	Timestamp time.Time `json:"time"`
+	//Def       string    `json:"status"`
 	Def string `json:"status"`
 }
 
@@ -50,6 +41,8 @@ type HttpResponse struct {
 
 type Items []Item
 
+var item []Item = arrDetails
+
 var x string
 var ii string
 var dataa = [1]int{}
@@ -65,6 +58,7 @@ var arrDetails []Item
 func f() {
 	go func() {
 		var s []Item
+
 		for {
 			file, err := os.Open("urls.txt")
 			if err != nil {
@@ -81,11 +75,19 @@ func f() {
 			file.Close()
 			ss = 0
 			for _, eachline := range txtlines {
+				//for i= 1; i< len(txtlines);i++{
 				ss++
+
 				var urls = []string{
 					eachline,
 				}
+				//chh := make(chan string)
 				results := asyncHttpGets(urls)
+				//	output = append(output, []string{<-chh})
+				//for _, value := range output {
+				//	fmt.Println(value)
+				//}
+
 				for _, result := range results {
 					fmt.Printf("%s status: %s\n", result.url, result.response.Status)
 					a = result.url
@@ -102,17 +104,33 @@ func f() {
 
 					}
 					fmt.Println("number of lines:", lineCount)
+					//	m := makeRange(1, lineCount+1)
+					//a := string(i)
 					var arrDetails []Item
+					//	arrDetails = append(arrDetails, []{<-chh})
 					for _, v := range dataa {
+						//	for i, line := range lines {
+						//	fmt.Println(i, line)
 						arrDetails = append(arrDetails, Item{
-							Id:  ss,
-							Url: eachline,
-							Timeresp: timeresp,
-							Num:      b,
+
+							Id:        ss,
+							Url:       eachline,
+							Timeresp:  timeresp,
+							Num:       b,
 							Timestamp: time.Now(),
 							Def:       fmt.Sprintf(c, v),
 						})
+
+						//}
 					}
+					/*lines, err := readLines("urls.txt")
+					if err != nil {
+						log.Fatalf("readLines: %s", err)
+					}
+					for i, line := range lines {
+						fmt.Println(i, line)
+						people = append(people, Person{ID: i})
+					}*/
 					file, _ = os.Open("urls.txt")
 					fileScanner = bufio.NewScanner(file)
 					lineCount = 0
@@ -122,69 +140,108 @@ func f() {
 
 					}
 					fmt.Println("number of lines:", lineCount)
+					//m := makeRange(1, lineCount+1)
+
 					s = append(s, arrDetails...)
+
 					if len(s) == (lineCount*1)+1 {
 						first := s[0]
 						fmt.Println(first)
 
 						s = s[1:]
 					}
+
 				}
+
 			}
 			data, err := json.Marshal(s)
 			if err != nil {
 				panic(err)
 			}
 
+			//fmt.Println(string(data))
 			x = string(data)
 			fmt.Println(string(data))
-			time.Sleep(5 * time.Second)
+
+			//defer file.Close()
+			time.Sleep(10 * time.Second)
 
 		}
 
 	}()
 
 }
+
 func main() {
 	go f()
-		mux := http.NewServeMux()
-		mux.HandleFunc("/api", Handlercheck)
 
-		handler := cors.Default().Handler(mux)
-		http.ListenAndServe(":10000", handler)
+	/*router := mux.NewRouter()
+
+	router.HandleFunc("/api/stats", Handlercheck).Methods("GET")
+	router.HandleFunc("/api/details/{id}", GetPersonEndpoint).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))*/
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api", Handlercheck)
+
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":10000", handler)
+
 }
 
 func Handlercheck(w http.ResponseWriter, r *http.Request) {
+	//bodyBytes, _ := ioutil.ReadAll(r.x)
+	//w.Write(bodyBytes)
+
 	fmt.Fprintf(w, x, html.EscapeString(r.URL.Path))
+
 }
-func fetch(url string, timestamp chan<- string) {
-	start := time.Now()
-	_, err := http.Get(url)
-	if err != nil {
-		timestamp <- fmt.Sprint(err)
-		return
+
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	id, _ := strconv.Atoi(params["id"])
+	for _, itemm := range item {
+		if itemm.Id == id {
+			data, err := json.Marshal(itemm)
+			if err != nil {
+				panic(err)
+			}
+
+			//json.NewEncoder(w).Encode(itemm)
+			fmt.Fprintf(w, string(data), html.EscapeString(req.URL.Path))
+			return
+		}
 	}
-
-	timestamp <- fmt.Sprintf("%s", start)
-
+	fmt.Fprintf(w, x, html.EscapeString(req.URL.Path))
+	//fmt.Fprintf(w, x, html.EscapeString(req.URL.Path))
+	//json.NewEncoder(w).Encode(&Item{})
 }
-func fetchcode(url string, statuscode chan<- string) {
+
+var output = make([][]string, 0)
+
+func MakeRequest(url string, ch chan<- string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		statuscode <- fmt.Sprint(err)
-		return
-	}
-
-	statuscode <- fmt.Sprintf("%d", resp.StatusCode)
+		ch <- fmt.Sprintf("url: %s, err: %s ", url, err)
+	} //else {
+	//	ch <- fmt.Sprintf("url: %s, status: %s ", url, resp.Status) // put response into a channel
+	defer resp.Body.Close()
+	//}
 }
+
 func asyncHttpGets(urls []string) []*HttpResponse {
-	ch := make(chan *HttpResponse, len(urls))
+	ch := make(chan *HttpResponse, len(urls)) // buffered
 	responses := []*HttpResponse{}
 	for _, url := range urls {
 		go func(url string) {
 			fmt.Printf("Fetching %s \n", url)
-
+			/*client := &http.Client{
+				CheckRedirect: func(req *http.Request, via []*http.Request) error {
+					return http.ErrUseLastResponse
+				},
+			}*/
 			resp, _ := http.Get(url)
+
 			ch <- &HttpResponse{url, resp, nil}
 		}(url)
 	}
@@ -206,24 +263,3 @@ func asyncHttpGets(urls []string) []*HttpResponse {
 
 }
 
-func makeRange(min, max int) []int {
-	a := make([]int, max-min+1)
-	for i := range a {
-		a[i] = min + i
-	}
-	return a
-}
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
